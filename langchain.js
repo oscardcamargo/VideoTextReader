5788da2efe567f5811b40fdd4cdd4e047b6a7dcf
JohnMcDonn <johnmcdonn@github.com>
2023-10-14 22:31:07 -0400

    Restrcutured for cleaner code by seperating tasks into distinct functions

--- langchain.js
+++ langchain.js
@@ -5,72 +5,52 @@ import { PromptTemplate } from "langchain/prompts";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import fs from "fs";
 
-async function mainFunction() {
+const run = async () => {
 
-// import AWS  from 'aws-sdk';
-// const textract = new AWS.Textract();
-
-// // Decode Base64 image
-// const base64Image = 'your_base64_encoded_image_string_here';
-// const decodedImage = Buffer.from(base64Image, 'base64');
-//
-// const params = {
-//     Document: {
-//         Bytes: decodedImage
-//     },
-//     FeatureTypes: ['TABLES', 'FORMS']
-// };
-//
-// textract.analyzeDocument(params, function(err, data) {
-//     if (err) console.log(err, err.stack); // an error occurred
-//     else     console.log(data);           // successful response
-// });
-
-/// AWS text extraction.
-    const run = async () => {
-
-        // Initialize Textract client
-        const client = new TextractClient({
-            region: "us-east-1",
-            credentials: {
-                secretAccessKey: process.env.AWS_API_SECRET,
-                accessKeyId: process.env.AWS_API_KEY,
-            }
-
-        });
-
-        // Read image file
-        const fileBuffer = fs.readFileSync("video_frame2.jpeg");
-
-        // Prepare command
-        const command = new DetectDocumentTextCommand({
-            Document: {
-                Bytes: fileBuffer,
-            },
-        });
-
-        try {
-            // Run Textract
-            const result = await client.send(command);
-
-            // Parse result
-            const rawText = result.Blocks.map((block) => block.Text).join(" ");
-            // const rawText = "print(\"hello world\")";
-            console.log("Extracted Text:", rawText);
-            return rawText;
-
-        } catch (err) {
-            console.error("An error occurred:", err);
-            return "error";
+    // Initialize Textract client
+    const client = new TextractClient({
+        region: "us-east-1",
+        credentials: {
+            secretAccessKey: process.env.AWS_API_SECRET,
+            accessKeyId: process.env.AWS_API_KEY,
         }
 
-    };
+    });
 
+    // Read image file
+    const fileBuffer = fs.readFileSync("video_frame2.jpeg");
 
+    // Prepare command
+    const command = new DetectDocumentTextCommand({
+        Document: {
+            Bytes: fileBuffer,
+        },
+    });
 
-//OpenAi ChatGpt interpretation of image
+    try {
+        // Run Textract
+        const result = await client.send(command);
+
+        // Parse result
+        // const rawText = "print(\"hello world\")";
+        // console.log("Extracted Text:", rawText);
+        return result.Blocks.map((block) => block.Text).join(" ");
+
+    } catch (err) {
+        console.error("An error occurred:", err);
+        return "error";
+    }
+
+};
+
+//summarizes videos
+async function reviewSession(){}
+async function frameExplanation(){
+
     const prompt1 = PromptTemplate.fromTemplate(
-        'Rewrite {extractedText} so that it is readible. Create a summary of the text. Write one short question based on the text. Output only what I ask and nothing else'
+
+
+        'Rewrite {extractedText} so that it is readible. Create a summary of the text. Mention 2-5 key insights from the text. Write one short practice question and answer based on the text. Output only what I ask and nothing else'
     );
 
     const model = new ChatOpenAI({
@@ -86,13 +66,16 @@ async function mainFunction() {
         extractedText: extractedText,
     });
 
-
-    const body_output = result.content
-    console.log(body_output)
-    return body_output
-
+    return result.content
 }
 
-console.log(mainFunction());
 
+
+async function mainFunction() {
+ const output = await frameExplanation();
+ console.log(output)
+    return output;
+}
+
+mainFunction();
 export { mainFunction };
